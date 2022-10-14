218cffdad20b5287ed48f10260e6e00631d66ed1
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-14 21:21:34 +0530

    formatting done

--- src/reducer/productReducer.js
+++ src/reducer/productReducer.js
@@ -1,56 +1,53 @@
-const ProductReducer = (state,action) =>{
+const ProductReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_LOADING":
+      return {
+        ...state,
+        isLoading: true,
+      };
 
-    switch (action.type) {
-        case "SET_LOADING":            
-           return{
-            ...state,
-            isLoading :true,    
-           };
+    case "SET_API_DATA":
+      const featureData = action.payload.filter((curElem) => {
+        return curElem.featured === true;
+      });
+      return {
+        ...state,
+        isLoading: false,
+        products: action.payload,
+        featureProducts: featureData,
+      };
 
-           case "SET_API_DATA":            
-             const featureData = action.payload.filter((curElem)=>{
-                return curElem.featured === true;
-             });
-             return{
-                ...state,
-                isLoading : false,
-                products: action.payload,
-                featureProducts : featureData,
-             }
+    case "API_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
 
+    case "SET_SINGLE_LOADING":
+      return {
+        ...state,
+        isSingleLoading: true,
+      };
 
-        case "API_ERROR":
-            return{
-                ...state,
-                isLoading:false,
-                isError : true,
-            };
+    case "SET_SINGLE_PRODUCT":
+      return {
+        ...state,
+        isSingleLoading: false,
+        singleProduct: action.payload,
+      };
 
-            case "SET_SINGLE_LOADING":            
-           return{
-            ...state,
-            isSingleLoading :true,    
-           };
+    case "SET_SINGLE_ERROR":
+      return {
+        ...state,
+        isSingleLoading: false,
+        isError: true,
+      };
 
-           case "SET_SINGLE_PRODUCT":
-            return{
-                ...state,
-            isSingleLoading :false,
-            singleProduct : action.payload,
-            };
+    default:
+      return state;
+  }
+  return state;
+};
 
-            case "SET_SINGLE_ERROR":
-                return{
-                    ...state,
-                    isSingleLoading: false,
-                    isError : true,
-                };
-    
-        default:
-            return state;
-            
-    }
-return state ;
-}
-
-export default  ProductReducer;
+export default ProductReducer;
