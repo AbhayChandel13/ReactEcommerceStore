3e2724be2e6ce0595ee6a2702f9b4b82bcb1c2a5
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-15 02:13:21 +0530

    working on all products page

--- src/Products.js
+++ src/Products.js
@@ -1,9 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import { useProductContext } from "./context/productcontext";
+import { Container } from "./styles/Container";
 
 const Products = () => {
-  return <Wrapper>
-      <h2 className="common-heading">products page </h2>
+  const {isLoading, products} = useProductContext();
+  console.log("products",products);
+  if(isLoading){
+    return  <div>....Loading </div>
+}
+  return <Wrapper className="section">      
+      <Container className="container">
+      <div className="grid grid-three-column">
+          {products.map((curElem) => {
+            <table>
+            <tr key={curElem.id} >
+              <td><th>Name</th></td>
+              <td><th>Company</th></td>
+              <td><th>Price</th></td>
+            </tr>
+            <tr>
+              <td>{curElem.name}</td>
+            </tr>            
+          </table>
+          })}
+          </div>
+        </Container>        
   </Wrapper>;
 };
 
