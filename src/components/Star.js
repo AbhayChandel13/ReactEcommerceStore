56c22df71a51db4bab37fd1905e270baa1d20db6
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-14 21:34:59 +0530

    formatting

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,28 +1,30 @@
-import {FaStar, FaStarHalfAlt} from "react-icons/fa";
-import {AiOutlineStar} from "react-icons/ai";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { AiOutlineStar } from "react-icons/ai";
 import styled from "styled-components";
 
-const Star = ({stars,reviews}) => {
-  const ratingstar =  Array.from({length:5},(elem,index)=>{
-
-        let number = index + 0.5;
-        return(
-            <span key={index}>
-                {
-                    stars>=index+1 ? <FaStar className="icon" /> : stars>=number ? <FaStarHalfAlt className="icon" /> : <AiOutlineStar className="icon" />
-                }
-            </span>
-        )
-    });
+const Star = ({ stars, reviews }) => {
+  const ratingstar = Array.from({ length: 5 }, (elem, index) => {
+    let number = index + 0.5;
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <FaStar className="icon" />
+        ) : stars >= number ? (
+          <FaStarHalfAlt className="icon" />
+        ) : (
+          <AiOutlineStar className="icon" />
+        )}
+      </span>
+    );
+  });
   return (
     <Wrapper>
-        <div className="icon-style">
-            {ratingstar}
-            <p>({reviews} customer reviews)</p>
-        </div>
-
+      <div className="icon-style">
+        {ratingstar}
+        <p>({reviews} customer reviews)</p>
+      </div>
     </Wrapper>
-  )
+  );
 };
 const Wrapper = styled.section`
 .icon-style{
@@ -46,4 +48,4 @@ const Wrapper = styled.section`
    
 `;
 
-export default Star
+export default Star;
