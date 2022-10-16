86813826f175a6ae4bcc422365f5fd9be20c469d
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-16 13:58:21 +0530

    Add quantity functionality added

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,9 +1,21 @@
 import React, { useState } from 'react'
 import styled from 'styled-components';
 import { FaCheck } from 'react-icons/fa';
+import CartAmountToggle from './CartAmountToggle';
+import { NavLink } from 'react-router-dom';
+import { Button } from "../styles/Button";
+
 const AddToCart = ({product}) => {
     const{id,colors,stock} = product;
     const [color, setColor] = useState(colors[0]);
+    const [amount,setAmount] = useState(1);
+
+    const setDecrease= ()=>{
+        amount > 1 ? setAmount(amount - 1) : setAmount(1);
+    }
+    const setIncrease=()=>{
+        amount < stock ? setAmount(amount + 1) : setAmount(stock);
+    }
   return (
     <Wrapper>
         <div className='colors'>
@@ -19,6 +31,12 @@ const AddToCart = ({product}) => {
                 })} 
             </p>
         </div>
+
+        <CartAmountToggle amount={amount} setDecrease ={setDecrease} setIncrease ={setIncrease} />
+
+        <NavLink to="/cart">
+        <Button className="btn">Add To Cart</Button>
+      </NavLink>
       
     </Wrapper>
   )
