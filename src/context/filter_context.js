ddac164fb4d9e1ec47a4d40910d2c1b5b978e8fb
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-21 00:42:09 +0530

    toggle in grid and list view

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -7,7 +7,7 @@ const FilterContext = createContext();
 const initialState ={
     filter_products:[],
     all_products:[],
-    grid_view: true,
+    grid_view: false,
 }
 
 export const FilterContextProvider = ({children})=>{
@@ -16,15 +16,22 @@ export const FilterContextProvider = ({children})=>{
     const [state,dispatch] = useReducer(reducer,initialState);
     //to set the Grid View 
     const setGridView= ()=>{
-        return dispatch({type: "SET_GRIDVIEW"});
+        return dispatch({type: "SET_GRID_VIEW"});
     }
+
+    //to set the List View 
+    const setListView = ()=>{
+        return dispatch({type: "SET_LIST_VIEW"});
+    }
+
+    
 
     useEffect(() => {
      dispatch({type: "LOAD_FILTER_PRODUCTS",payload: products});
     }, [products])
     
      return(
-        <FilterContext.Provider value={{...state}}>
+        <FilterContext.Provider value={{...state,setGridView,setListView}}>
             {children}
         </FilterContext.Provider>
      )
@@ -32,4 +39,4 @@ export const FilterContextProvider = ({children})=>{
 
 export const useFilterContext =()=>{
     return useContext(FilterContext);
-}
+}   
--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -7,11 +7,17 @@ const filterReducer = (state,action)=> {
                     all_products: [...action.payload]
                 };
 
-            case "SET_GRIDVIEW":
+            case "SET_GRID_VIEW":
                 return{
                     ...state,
                     grid_view :true,
                 }
+
+            case "SET_LIST_VIEW":
+                return{
+                    ...state,
+                    grid_view :false,
+                } 
 
             default:
                 return state;
