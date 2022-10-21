c2e34f18dce19375bd89f8e67cf3ac10d4bc40a3
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-22 01:15:53 +0530

    code formatting

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -1,40 +1,36 @@
-const filterReducer = (state,action)=> {
-        switch(action.type){
-            case "LOAD_FILTER_PRODUCTS" :
-                return {
-                    ...state,
-                    filter_products: [...action.payload],
-                    all_products: [...action.payload]
-                };
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case "LOAD_FILTER_PRODUCTS":
+      return {
+        ...state,
+        filter_products: [...action.payload],
+        all_products: [...action.payload],
+      };
 
-            case "SET_GRID_VIEW":
-                return{
-                    ...state,
-                    grid_view :true,
-                }
+    case "SET_GRID_VIEW":
+      return {
+        ...state,
+        grid_view: true,
+      };
 
-            case "SET_LIST_VIEW":
-                return{
-                    ...state,
-                    grid_view :false,
-                } 
+    case "SET_LIST_VIEW":
+      return {
+        ...state,
+        grid_view: false,
+      };
 
-            case "GET_SORT_VALUE":
-                let userSortValue = document.getElementById("sort");
-                let sort_value = userSortValue.options[userSortValue.selectedIndex].value;
-                console.log(sort_value);
-                return{
-                    ...state,
-                    sorting_value: sort_value,
-                };
+    case "GET_SORT_VALUE":
+      let userSortValue = document.getElementById("sort");
+      let sort_value = userSortValue.options[userSortValue.selectedIndex].value;
+      console.log(sort_value);
+      return {
+        ...state,
+        sorting_value: sort_value,
+      };
 
-            default:
-                return state;
-
-        }
- 
-}
-
-
+    default:
+      return state;
+  }
+};
 
 export default filterReducer;
