d4ca81f74951814299ca266ce2ace7fe8976038d
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-22 01:14:59 +0530

    formatting done

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -8,10 +8,10 @@ const initialState = {
   filter_products: [],
   all_products: [],
   grid_view: true,
-  sorting_value:"lowest",
+  sorting_value: "lowest",
 };
 
-export const FilterContextProvider = ({ children }) => {    
+export const FilterContextProvider = ({ children }) => {
   const { products } = useProductContext();
   const [state, dispatch] = useReducer(reducer, initialState);
   //to set the Grid View
@@ -23,20 +23,22 @@ export const FilterContextProvider = ({ children }) => {
   const setListView = () => {
     return dispatch({ type: "SET_LIST_VIEW" });
   };
-  const sorting=()=>{
-    dispatch({type: "GET_SORT_VALUE"});
-  }
+  const sorting = () => {
+    dispatch({ type: "GET_SORT_VALUE" });
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     sorting();
-  },[state.sorting_value]);
+  }, [state.sorting_value]);
 
   useEffect(() => {
     dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
 
   return (
-    <FilterContext.Provider value={{ ...state, setGridView, setListView,sorting }}>
+    <FilterContext.Provider
+      value={{ ...state, setGridView, setListView, sorting }}
+    >
       {children}
     </FilterContext.Provider>
   );
