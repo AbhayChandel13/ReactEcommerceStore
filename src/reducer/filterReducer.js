0a674ec24a0cd4d392d6a7163bd65d1f1dd5f6a1
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-26 21:16:55 +0530

    code formatting

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -25,42 +25,42 @@ const filterReducer = (state, action) => {
       console.log(sort_value);
       return {
         ...state,
-        sorting_value : sort_value,
+        sorting_value: sort_value,
       };
 
-      case "SORTING_PRODUCTS":
-        let newSortData;
-        let tempSortProducts = [...action.payload];
+    case "SORTING_PRODUCTS":
+      let newSortData;
+      let tempSortProducts = [...action.payload];
 
-        if(state.sorting_value === "lowest"){
-          const sortingProducts =(a,b)=>{
+      if (state.sorting_value === "lowest") {
+        const sortingProducts = (a, b) => {
           return a.price - b.price;
-          };  
-          newSortData = tempSortProducts.sort(sortingProducts);
-        }
-        if(state.sorting_value === "highest"){
-          const sortingProducts =(a,b)=>{
+        };
+        newSortData = tempSortProducts.sort(sortingProducts);
+      }
+      if (state.sorting_value === "highest") {
+        const sortingProducts = (a, b) => {
           return b.price - a.price;
-          };  
-          newSortData = tempSortProducts.sort(sortingProducts);
-        }
+        };
+        newSortData = tempSortProducts.sort(sortingProducts);
+      }
 
-        if(state.sorting_value === "a-z"){
-          newSortData = tempSortProducts.sort((a,b)=>
-           a.name.localeCompare(b.name)
-          )
-        }
+      if (state.sorting_value === "a-z") {
+        newSortData = tempSortProducts.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+      }
 
-        if(state.sorting_value === "z-a"){
-          newSortData = tempSortProducts.sort((a,b)=>
-           b.name.localeCompare(a.name)
-          )
-        }
+      if (state.sorting_value === "z-a") {
+        newSortData = tempSortProducts.sort((a, b) =>
+          b.name.localeCompare(a.name)
+        );
+      }
       return {
         ...state,
-        filter_products : newSortData,
+        filter_products: newSortData,
       };
-  
+
     default:
       return state;
   }
