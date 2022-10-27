16de373717318b714e1e5919e3bcc4d4d5e3fca1
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-27 21:02:04 +0530

    code formatting

--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -1,24 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-
 const FilterSection = () => {
-  return <Wrapper>
-    <div className="filter-search">
-      <form onSubmit={(e)=>e.preventDefault()}>
-        <input
-        type="text"
-        name="text"
-        //value={text}
-       //onChange={updateFilterValue}
-        />
-      </form>
-    </div>
-
-  </Wrapper>
+  return (
+    <Wrapper>
+      <div className="filter-search">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            name="text"
+            //value={text}
+            //onChange={updateFilterValue}
+          />
+        </form>
+      </div>
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.section`
-
-`;
+const Wrapper = styled.section``;
 export default FilterSection;
