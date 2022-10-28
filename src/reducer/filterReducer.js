b2748e8f2fb50c25a6fba55d4301ccdf8777919e
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-29 03:59:27 +0530

    code format

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -69,22 +69,22 @@ const filterReducer = (state, action) => {
         },
       };
 
-      case "FILTER_PRODUCTS":
-        let{ all_products} = state;
-        let tempFilterProduct = [...all_products];
+    case "FILTER_PRODUCTS":
+      let { all_products } = state;
+      let tempFilterProduct = [...all_products];
 
-        const {text} = state.filters;
+      const { text } = state.filters;
 
-        if(text){
-          tempFilterProduct = tempFilterProduct.filter((curElem)=>{
-            return curElem.name.toLowerCase().includes(text);
-          });
-        }
+      if (text) {
+        tempFilterProduct = tempFilterProduct.filter((curElem) => {
+          return curElem.name.toLowerCase().includes(text);
+        });
+      }
 
-        return{
-          ...state,
-          filter_products: tempFilterProduct  ,
-        }
+      return {
+        ...state,
+        filter_products: tempFilterProduct,
+      };
     default:
       return state;
   }
