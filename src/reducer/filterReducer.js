8c9ccd34507d9cf43e102903620fa945de0732f2
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-11-11 20:12:04 +0530

    code format

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -73,7 +73,7 @@ const filterReducer = (state, action) => {
       let { all_products } = state;
       let tempFilterProduct = [...all_products];
 
-      const { text,category } = state.filters;
+      const { text, category } = state.filters;
 
       if (text) {
         tempFilterProduct = tempFilterProduct.filter((curElem) => {
