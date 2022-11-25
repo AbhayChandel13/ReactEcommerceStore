9df995edcdc31cb32935e7603f883d6e94fc0449
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-11-26 01:35:20 +0530

    code format

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,26 +1,30 @@
-import {createContext,useReducer,useContext} from "react";
-import reducer from   "../reducer/cartReducer";
+import { createContext, useReducer, useContext } from "react";
+import reducer from "../reducer/cartReducer";
 
 const CartContext = createContext();
 
-const intialState ={
-    cart:[],
-    total_item:"",
-    total_amount:"",
-    shipping_fee: 50000,
+const intialState = {
+  cart: [],
+  total_item: "",
+  total_amount: "",
+  shipping_fee: 50000,
 };
 
-const CartProvider = ({children}) =>{
-    const  [state,dispatch] = useReducer(reducer, intialState);
+const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, intialState);
 
-    const addToCart = (id,color, amount, product)=>{
-        dispatch({type:"ADD_TO_CART",payload:{id,color, amount, product}})
-    }
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
+  };
 
-    return <CartContext.Provider value={{...state,addToCart}}>{children}</CartContext.Provider>
+  return (
+    <CartContext.Provider value={{ ...state, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
-const  useCartContext = () =>{
-    return useContext(CartContext);
-}
+const useCartContext = () => {
+  return useContext(CartContext);
+};
 
-export  {CartProvider,useCartContext};
+export { CartProvider, useCartContext };
