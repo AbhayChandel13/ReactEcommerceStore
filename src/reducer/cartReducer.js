473905348684fce0ddee5c07387be7557b9efaf8
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-12-15 20:47:21 +0530

    code format

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,36 +1,35 @@
-const cartReducer = (state,action) => {
-    if(action.type === "ADD_TO_CART"){
-        let{id,color, amount, product}= action.payload;
-        console.log(product);
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_TO_CART") {
+    let { id, color, amount, product } = action.payload;
+    console.log(product);
 
-      let cartProduct;
+    let cartProduct;
 
-      cartProduct={
-        id:id+color,
-        name : product.name,
-        color,
-        amount,
-        image:product.image[0].url,
-        price:product.price,
-        max:product.stock,
-      };
-      return{
-        ...state,
-        cart:[...state.cart, cartProduct],
-      };
+    cartProduct = {
+      id: id + color,
+      name: product.name,
+      color,
+      amount,
+      image: product.image[0].url,
+      price: product.price,
+      max: product.stock,
+    };
+    return {
+      ...state,
+      cart: [...state.cart, cartProduct],
+    };
+  }
 
-    }
-
-    if(action.type=== "REMOVE_ITEM"){
-      let updatedCart = state.cart.filter(
-        (curItem)=> curItem.id !== action.payload
-      );
-      return{
-        ...state,
-        cart: updatedCart,
-      }
-    }
+  if (action.type === "REMOVE_ITEM") {
+    let updatedCart = state.cart.filter(
+      (curItem) => curItem.id !== action.payload
+    );
+    return {
+      ...state,
+      cart: updatedCart,
+    };
+  }
   return state;
-}
+};
 
-export default cartReducer
+export default cartReducer;
