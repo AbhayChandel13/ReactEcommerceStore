fd789aa5e743ad53e962da6f990c1b2bd2017c29
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-12-16 00:45:24 +0530

    C.D.

--- src/styles/Button.js
+++ src/styles/Button.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
- 
   text-decoration: none;
   max-width: auto;
   background-color: rgb(98 84 243);
@@ -15,7 +14,7 @@ export const Button = styled.button`
   -webkit-transition: all 0.3s ease 0s;
   -moz-transition: all 0.3s ease 0s;
   -o-transition: all 0.3s ease 0s;
-  
+
   &:hover,
   &:active {
     box-shadow: 0 2rem 2rem 0 rgb(132 144 255 / 30%);
