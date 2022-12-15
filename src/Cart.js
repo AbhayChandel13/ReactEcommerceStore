ec34aac87c16d67557b4288bb882ba5390aced7b
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-12-15 20:47:53 +0530

    code format

--- src/Cart.js
+++ src/Cart.js
@@ -3,25 +3,27 @@ import { useCartContext } from "./context/cart_context";
 import CartItem from "./components/CartItem";
 
 const Cart = () => {
-  const {cart} = useCartContext();
-  console.log("Cart",cart);
-  return <Wrapper>
-    <div className="container"> 
-    <div className="cart_heading grid grid-five-column">
-      <p> Item </p> 
-      <p className="cart-hide"> Price</p>
-      <p> Quantity </p>
-      <p className="cart-hide"> Subtotal</p>
-      <p> Remove </p>
-    </div>
-    <hr />
-      <div className="cart-item">
-        {cart.map((curElem)=>{
-          return <CartItem key={curElem.id} {...curElem} />;
-        })}
-      </div> 
-    </div>
-  </Wrapper>;
+  const { cart } = useCartContext();
+  console.log("Cart", cart);
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="cart_heading grid grid-five-column">
+          <p> Item </p>
+          <p className="cart-hide"> Price</p>
+          <p> Quantity </p>
+          <p className="cart-hide"> Subtotal</p>
+          <p> Remove </p>
+        </div>
+        <hr />
+        <div className="cart-item">
+          {cart.map((curElem) => {
+            return <CartItem key={curElem.id} {...curElem} />;
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
