66b99907b8eeb4d4088b6a97018ce851abac5673
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-12-23 20:17:04 +0530

    comments

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -19,7 +19,7 @@ const cartReducer = (state, action) => {
       cart: [...state.cart, cartProduct],
     };
   }
-  //Remove or disc Items From Cart
+  //Remove or discard Items From Cart
   if (action.type === "REMOVE_ITEM") {
     let updatedCart = state.cart.filter(
       (curItem) => curItem.id !== action.payload
