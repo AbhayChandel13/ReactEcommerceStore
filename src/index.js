2e021678f2c8454ab902efec3c44ef9ce2f96d1d
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-01-06 02:12:50 +0530

    Env variables protected

--- src/index.js
+++ src/index.js
@@ -9,21 +9,25 @@ import { CartProvider } from "./context/cart_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// const domain = process.env.REACT_APP_AUTH_DOMAIN;
+// const clientId = process.env.REACT_APP_CLIENT_ID;
 
 root.render(
+  
   <AppProvider>
     <FilterContextProvider>
       <CartProvider>
-        <Auth0Provider
+      <Auth0Provider
           domain="dev-dm7r081voxlt31qx.us.auth0.com"
           clientId="n9PGAKaaeyJrtlOJWmghHhJErcBmrMd0"
           redirectUri={window.location.origin}
         >
           <App />
-        </Auth0Provider>
+          </Auth0Provider>
       </CartProvider>
     </FilterContextProvider>
   </AppProvider>
+  
 );
 
 reportWebVitals();
