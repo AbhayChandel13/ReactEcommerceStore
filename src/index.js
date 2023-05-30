1e970f7238c96f3800d7fe27639c0c112ffd34f1
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-05-30 20:07:32 +0530

    comment deleted

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // const domain = process.env.REACT_APP_AUTH_DOMAIN;
 // const clientId = process.env.REACT_APP_CLIENT_ID;
-// const domain = process.env.REACT_APP_AUTH_DOMAIN;
+
 
 
 root.render(
