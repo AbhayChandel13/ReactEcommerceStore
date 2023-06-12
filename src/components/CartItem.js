becb6ab9b3af079c7b1f1d6f4b01d7a9fe0062bb
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-06-12 19:59:51 +0530

    edited

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -13,7 +13,6 @@ const CartItem = ({ id, name, image, color, price, amount }) => {
   // const setIncrease = () => {
   //    amount < stock ? setAmount(amount + 1) : setAmount(stock);
   // };
-  // const setIncrease = () => {
 
   return (
     <div className="cart_heading grid grid-five-column">
