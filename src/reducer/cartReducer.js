7498eca453a3457178315b995cd74efa182d9d1d
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-06-30 05:20:01 +0530

    deleted

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -107,11 +107,6 @@ const cartReducer = (state, action) => {
   //     return initialVal;
   //   },0);
 
-  //   return{
-  //     ...state,
-  //     total_item : updatedItemVal,
-  //   }
-
   // }
     //   return{
   //     ...state,
