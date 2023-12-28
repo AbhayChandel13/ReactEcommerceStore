ff328954f8886e8ac744ae1cfe2614a5dc07b3f7
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-12-29 03:27:43 +0530

    comments edited

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -27,7 +27,7 @@ const intialState = {
 const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, intialState);
 
-  //Add Items to the cart:
+  //Adding Items to the cart:
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
   };
