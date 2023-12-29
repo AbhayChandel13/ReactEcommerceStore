8b466a2849e46c4afa2529e6bedcd457780ce77a
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-12-30 01:40:19 +0530

    comment

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -44,7 +44,7 @@ const cartReducer = (state, action) => {
     }
   }
 
-  //To set the Increment and decrement func 
+  //To set    the Increment   and decrement functions
   if (action.type === "SET_DECREMENT") {
     let updatedProduct = state.cart.map((curElem) => {
       if (curElem.id === action.payload) {
