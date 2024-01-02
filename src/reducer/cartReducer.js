59fb0e8111fd80b901c1f66da2cc3252ac06095c
AbhayChandel13 <chandelabhay4515@gmail.com>
2024-01-02 20:17:47 +0530

    comment

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -128,6 +128,8 @@ const cartReducer = (state, action) => {
   //   }
   // }
 
+  //comments only
+
   if (action.type === "CART_ITEM_PRICE_TOTAL") {
     let { total_item, total_price } = state.cart.reduce(
       (accum, curElem) => {
