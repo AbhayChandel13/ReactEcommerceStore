8bacea57060954963b0241ccbcd8621b30dbe29d
AbhayChandel13 <chandelabhay4515@gmail.com>
2024-02-14 21:54:48 +0530

    commit

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -99,7 +99,8 @@ const cartReducer = (state, action) => {
       cart: [],
     };
   }
-
+  
+  //
   // if(action.type === "CART_TOTAL_ITEM"){
   //   let updatedItemVal = state.cart.reduce((initialVal,curElem)=>{
   //     let {amount} = curElem;
